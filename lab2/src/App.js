57f3d8689d8c110034111e141fcaa56030fb780c
chokladio <chokladio@gmail.com>
2019-02-07 00:29:50 +0100

    Checkboxes and basic layout

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
@@ -9,33 +8,20 @@ class App extends Component {
     return (<div>
       <div className="jumbotron jumbotron-fluid">
         <div class="container">
-          <div class="row">
-            <h1 class="display-4">My Own Salad Bar</h1>
-            <p class="lead">Here you can order custom-made salads!</p>
+          <div class="col-md">
+            <div class="row">
+              <h1 class="display-4">Sallad? Sallad!</h1>
+            </div>
+            <div class="row">
+              <p class="lead">"Man säger ju aldrig nej till lite sallad"</p>
+            </div>
           </div>
         </div>
       </div>
 
       <ComposeSalad inventory={inventory}/>
 
-      <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            ett
-          </div>
-          <div class="col-sm">
-            två
-          </div>
-          <div class="col-sm">
-            tre
-          </div>
-          <div class="col-sm">
-            fyra
-          </div>
-        </div>
-      </div>
-
-    </div>);
+  </div>);
   }
 }
 
--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -4,20 +4,69 @@ class ComposeSalad extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    render() {
-      const inventory = this.props.inventory;
-      let foundations = Object.keys(inventory).filter(name => inventory[name].foundation);
-      return (
-        <div className="container">
-          <ul>
-            {foundations.map(name => <li key={name}> {name}</li>)}
-          </ul>
-        </div>
-        <span>
-          {/* this part do not do anything right now */}
-        </span>
-      );
-    }
   }
+  render() {
+    const inventory = this.props.inventory;
+    let foundations = Object.keys(inventory).filter(name => inventory[name].foundation);
+    let proteins = Object.keys(inventory).filter(name => inventory[name].protein);
+    let extras = Object.keys(inventory).filter(name => inventory[name].extra);
+    let dressings = Object.keys(inventory).filter(name => inventory[name].dressing);
+    return (<div className="container">
+      <form onSubmit={this.handleSubmit}>
+        <div class="row justify-content-center mt-5">
+          <h5>Välj en bas</h5>
+          <div class="w-100"></div>
+          <div class="col-xl-6 col-md-8 col-sm-10 col-11">
+            {
+              foundations.map(name => <label class="form-check-label col-md-4 col-sm-6 col">
+                <input type="checkbox" class="form-check-input" value={this.state.value} onChange={this.handleChange}/>
+                <span>{name}</span>
+              </label>)
+            }
+          </div>
+        </div>
+
+        <div class="row justify-content-center mt-5">
+          <h5>Välj protein</h5>
+          <div class="w-100"></div>
+          <div class="col-xl-6 col-md-8 col-sm-10 col-11">
+            {
+              proteins.map(name => <label class="form-check-label col-md-4 col-sm-6 col">
+                <input type="checkbox" class="form-check-input" value={this.state.value} onChange={this.handleChange}/>
+                <span>{name}</span>
+              </label>)
+            }
+          </div>
+        </div>
+
+        <div class="row justify-content-center mt-5">
+          <h5>Välj extra ingredienser</h5>
+          <div class="w-100"></div>
+          <div class="col-xl-6 col-md-8 col-sm-10 col-11">
+            {
+              extras.map(name => <label class="form-check-label col-md-4 col-sm-6 col">
+                <input type="checkbox" class="form-check-input" value={this.state.value} onChange={this.handleChange}/>
+                <span>{name}</span>
+              </label>)
+            }
+          </div>
+        </div>
+
+        <div class="row justify-content-center mt-5">
+          <h5>Välj en dressing</h5>
+          <div class="w-100"></div>
+          <div class="col-xl-6 col-md-8 col-sm-10 col-11">
+            {
+              dressings.map(name => <label class="form-check-label col-md-4 col-sm-6 col">
+                <input type="checkbox" class="form-check-input" value={this.state.value} onChange={this.handleChange}/>
+                <span class="px-2 py-1">{name}</span>
+              </label>)
+            }
+          </div>
+        </div>
+      </form>
+    </div>);
+  }
+}
 
 export default ComposeSalad;
