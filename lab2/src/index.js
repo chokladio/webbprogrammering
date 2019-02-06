540927e07154fc59267cc02254a1529a68a798f0
chokladio <chokladio@gmail.com>
2019-02-06 17:48:19 +0100

    lab2 default setup

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.js"; // skip this if you do not use bootstrap modals
+import $ from 'jquery';                  // skip this if you do not use bootstrap modals
+import Popper from 'popper.js';          // skip this if you do not use bootstrap modals
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
